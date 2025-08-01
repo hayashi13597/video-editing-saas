86449487694c5aab300705d198a9796d9e28558b
hophuclam <hpl13597@gmail.com>
2025-08-01 09:21:36 +0700

    feat: Create ProjectsList component and update metadata type in Projects page

--- src/app/dashboard/projects/list/page.tsx
+++ src/app/dashboard/projects/list/page.tsx
@@ -4,17 +4,17 @@ import { Metadata } from "next";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "案件一覧",
+  title: "応募可能な案件一覧",
   description:
-    "案件一覧ページです。ここでは、プロジェクトの詳細を確認できます。"
+    "応募可能な案件一覧ページです。ここでは、応募可能なプロジェクトの詳細を確認できます。"
 };
 
-const Projects = () => {
+const ProjectsList = () => {
   return (
     <Suspense fallback={<ProjectsListSkeleton />}>
       <MainProjects />
     </Suspense>
-  );
-};
+  )
+}
 
-export default Projects;
+export default ProjectsList
