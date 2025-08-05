979b872a51ae8d791d84e963d6a001f098e2ccde
hophuclam <hpl13597@gmail.com>
2025-08-05 16:57:14 +0700

    feat: enhance FeedbacksList and VideoVersionList components with structured feedback display and version management

--- src/features/dashboard/projects/details/components/FeedbacksList.tsx
+++ src/features/dashboard/projects/details/components/FeedbacksList.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from "@/components/ui/accordion";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
 
 const FeedbacksList = () => {
-  return <div>FeedbacksList</div>;
+  return (
+    <div className="space y-3">
+      <h3 className="small-title">フィードバック一覧</h3>
+      <Accordion
+        type="single"
+        collapsible
+        className="w-full space-y-1"
+        defaultValue="task-1"
+      >
+        <AccordionItem value="task-1" className="border-none">
+          <AccordionTrigger className="bg-light-gray hover:no-underline px-6 py-2.5 body-text-bold [&_svg]:text-text items-center">
+            <div className="flex flex-col">
+              <span className="intro-text">6点修正お願いします</span>
+              <span className="text-gray">6/6</span>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className="mt-2.5">
+            <div className="space-y-2.5">
+              <div className="flex items-center gap-6 text-balance px-2.5">
+                <Checkbox id="content-1" />
+                <Label htmlFor="content-1" className="flex flex-col items-start">
+                  <span>1. 商品紹介ページのテロップのズレ</span>
+                  <span className="small-text text-gray">1:33</span>
+                </Label>
+              </div>
+              <div className="flex items-center gap-6 text-balance px-2.5">
+                <Checkbox id="content-2" />
+                <Label htmlFor="content-2" className="flex flex-col items-start">
+                  <span>2. 背景に会社のロゴを載せる</span>
+                  <span className="small-text text-gray">2:30</span>
+                </Label>
+              </div>
+              <div className="flex items-center gap-6 text-balance px-2.5">
+                <Checkbox id="content-3" />
+                <Label htmlFor="content-3" className="flex flex-col items-start">
+                  <span>3. テロップとBGM 修正のお願い</span>
+                  <span className="small-text text-gray">2:59</span>
+                </Label>
+              </div>
+              <div className="flex items-center gap-6 text-balance px-2.5">
+                <Checkbox id="content-4" />
+                <Label htmlFor="content-4" className="flex flex-col items-start">
+                  <span>4. 全体的な色を薄い青色にしたい</span>
+                  <span className="small-text text-gray">3:10</span>
+                </Label>
+              </div>
+              <div className="flex items-center gap-6 text-balance px-2.5">
+                <Checkbox id="content-5" />
+                <Label htmlFor="content-5" className="flex flex-col items-start">
+                  <span>5. ui/uxをUI/UXに変更</span>
+                  <span className="small-text text-gray">3:40</span>
+                </Label>
+              </div>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
 };
 
 export default FeedbacksList;
--- src/features/dashboard/projects/details/components/VideoVersionList.tsx
+++ src/features/dashboard/projects/details/components/VideoVersionList.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const VideoVersionList = () => {
-  return <div>VideoVersionList</div>;
+  return (
+    <div className="space y-3">
+      <h3 className="small-title">バージョン管理</h3>
+      <div className="space-y-1">
+        <div className="flex-between bg-light-green p-2.5 rounded-6">
+          <span className="text-green-main">ver_03.mp4</span>
+          <span className="very-small-text">2025/07/01</span>
+        </div>
+        <div className="flex-between bg-light-green p-2.5 rounded-6">
+          <span className="text-green-main">ver_02.mp4</span>
+          <span className="very-small-text">2015/06/01</span>
+        </div>
+        <div className="flex-between bg-light-green p-2.5 rounded-6">
+          <span className="text-green-main">ver_01.mp4</span>
+          <span className="very-small-text">2015/05/01</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoVersionList;
