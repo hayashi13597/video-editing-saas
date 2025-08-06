61e608c398b74838bd8a7721013a6e84b60f50ba
hophuclam <hpl13597@gmail.com>
2025-08-06 11:07:04 +0700

    feat: consolidate sign-up functionality into a single SignUpFormPage component; remove individual client and freelancer pages

---
+++ src/app/(auth)/sign-up/[role]/page.tsx
@@ -0,0 +1,30 @@
+import SignUpForm from "@/features/auth/sign-up/SignUpForm";
+import { notFound } from "next/navigation";
+
+interface SignUpFormPageProps {
+  params: {
+    role: string;
+  };
+}
+
+const SignUpFormPage = async ({ params }: SignUpFormPageProps) => {
+  const { role } = await params;
+
+  if (!["client", "freelancer"].includes(role)) {
+    notFound();
+  }
+
+  return <SignUpForm />;
+};
+
+export default SignUpFormPage;
+
+// generateMetadata
+export const generateMetadata = async ({ params }: SignUpFormPageProps) => {
+  const { role } = await params;
+
+  return {
+    title: `${role === "freelancer" ? "フリーランサー" : "クライアント"}登録`,
+    description: `新しい${role === "freelancer" ? "フリーランサー" : "クライアント"}アカウントを作成する`
+  }
+}
